efb1b8c408e32d8883734e14ed875b11dc03578c
Roberto Pinho <roberto.pinho@protonmail.com>
2025-12-30 15:31:51 -0300

    Add interactive greeting contract

---
+++ interactiveGreeting.sol
@@ -0,0 +1,12 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract InteractiGreeting{
+
+    string public greet = "Blocktest";
+
+    function setGreeting(string memory newGreeting) public {
+        greet = newGreeting;
+    }
+    
+}
